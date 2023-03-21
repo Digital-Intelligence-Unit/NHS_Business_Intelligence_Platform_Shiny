3f28b5a2287e32b33f1a899be312a793a9fce16d
philowhitmarsh <116095572+philowhitmarsh@users.noreply.github.com>
2023-03-21 14:46:25 +0000

    Update clustering.r

--- mountpoints/apps/Machine_Learning_Companion/app/tabs/clustering.r
+++ mountpoints/apps/Machine_Learning_Companion/app/tabs/clustering.r
@@ -1,3 +1,4 @@
+
 clustering_cols = c("Risk Score"                              , "Risk Score Rank" ,                        
 "Risk Score Trend"                        , "Risk Score Group"  ,                      
 "Risk Score Int"                          , "Risk Segment" ,                           
@@ -35,10 +36,12 @@ clustering_cols = c("Risk Score"                              , "Risk Score Rank
 "ethniccategory"                          , "du"  ,                                    
 "electoral ward or division")
 
+
+
 clustering <- tabPanel("Clustering", fluid = TRUE,
                      mainPanel(
                        fluidRow(
-                         column(width = 9,
+                         column(width = 7,
                                 fluidRow(
                                   box(width = 12, title = "Variable Setup",
                                       column(width = 6,
@@ -82,13 +85,7 @@ clustering <- tabPanel("Clustering", fluid = TRUE,
                                                                actionButton("kmgosh",label = "silhouette"),
                                                                bsTooltip("kmgosh", "Used to evaluate the quality of clusters. Can take some time to process.")
                                                )
-                                               ,fluidRow(
-                                                  uiOutput("plot1")
-                                                        )
-                                               ,fluidRow(
-                                                      br(),br(),
-                                                      uiOutput("plot2")
-                                                      )
+                                               
                                               #  ,fluidRow(
                                               #         br(),br()
                                               #    ,uiOutput("plot5")
@@ -100,23 +97,24 @@ clustering <- tabPanel("Clustering", fluid = TRUE,
                                                  )
                                              ))))),
                          
-                         column(width = 3,
-                                fluidRow(
-                                  div(
-                                    box(width = 12, id = "kmInfo",style = "overflow-y: scroll;", title = "PAM Clustering",
-                                        column(width = 12,
-                                               fluidRow(
-                                                 p("PAM (partition around medoids) clustering, is an iterative, data-partitioning algorithm that assigns n observations to exactly one of k clusters defined by medoids, where k is chosen before the algorithm starts."),
-                                                 p("See ", a("PAM", href="https://www.cs.umb.edu/cs738/pam1.pdf"), " for more information."),
-                                                 p("Why use PAM?"),
-                                                 p("The PAM clustering algorithm is used to find groups which have not been explicitly labeled in the data. This can be used to confirm business assumptions about what types of groups exist or to identify unknown groups in complex data sets."),
-                                                 p("See ", a("Introduction to PAM Clustering", href="https://www.datanovia.com/en/lessons/k-medoids-in-r-algorithm-and-practical-examples/"), " for more information."),
-                                               )
-                                        )
-                                    ), style = "position:fixed;  width:inherit;")
-                                )
-                         )
+                         
+           column(width = 5,
+       fluidRow(
+         column(width = 12,
+                div(
+                  style = "width: 100%; height: 100%;",
+                  fluidRow(
+                                                  uiOutput("plot1")
+                                                        )
+                                               ,fluidRow(
+                                                      br(),br(),
+                                                      uiOutput("plot2")
+                                                      )
+                )
+         )
+       )
+)
                        )
-
                      )
-            )
+)
+           
