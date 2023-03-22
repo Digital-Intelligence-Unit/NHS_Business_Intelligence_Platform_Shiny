984f2922e67bd6acbffbd8c62f784671e00a242a
philowhitmarsh <116095572+philowhitmarsh@users.noreply.github.com>
2023-03-22 13:26:05 +0000

    Update welcome.r

--- mountpoints/apps/Machine_Learning_Companion/app/tabs/welcome.r
+++ mountpoints/apps/Machine_Learning_Companion/app/tabs/welcome.r
@@ -12,6 +12,7 @@ welcome <-tabPanel(
             p("Classification and Regression Tree (CART), referred to as 'Decision Trees' on this page, is a data-driven regression technique."),
             p("See ", a("Tree-Based Models", href="https://blog.dataiku.com/tree-based-models-how-they-work-in-plain-english"), " for how regression trees work."),
             p("Use decision trees either to segment the population, or to discover what attributes are most statically significant in explaining differences (of the selected target variable).")
+            ,actionButton("tab1_button12", "View Decision Tree")
           )
         ),
         column(width = 6,
@@ -25,6 +26,8 @@ welcome <-tabPanel(
             p("Generalized Linear Models (GLM), a generalization of ordinary linear regression that allows for response variables that have error distribution models other than a normal distribution like Gaussian distribution."),
             p("See ", a("GLM in R", href="https://www.datacamp.com/tutorial/generalized-linear-models"), " for a tutorial."),
             p("GLM models allow us to build a linear relationship between the response and predictors, even though their underlying relationship is not linear. This is made possible by using a link function, which links the response variable to a linear model.")
+          ,actionButton("tab2_button22", "View GLM")
+          
           )
         )
       ),
@@ -40,6 +43,7 @@ welcome <-tabPanel(
             p("A Bayesian network is a probabilistic graphical model that represents a set of variables and their conditional dependencies via a directed acyclic graph."),
             p("See ", a("Bayesian network", href="https://en.wikipedia.org/wiki/Bayesian_network"), " for more information."),
             p("Bayesian networks are a type of Probabilistic Graphical Model that can be used to build models from data and/or expert opinion. They can be used for a wide range of tasks including diagnostics, reasoning, causal modeling, decision making under uncertainty, anomaly detection, automated insight and prediction.")
+            ,actionButton("tab3_button32", "View Bayesian Network")
           )
         ),
 
@@ -56,6 +60,7 @@ welcome <-tabPanel(
             p("Why use PAM?"),
             p("The PAM clustering algorithm is used to find groups which have not been explicitly labeled in the data. This can be used to confirm business assumptions about what types of groups exist or to identify unknown groups in complex data sets."),
             p("See ", a("Introduction to PAM Clustering", href="https://www.datanovia.com/en/lessons/k-medoids-in-r-algorithm-and-practical-examples/"), " for more information.")
+            ,actionButton("tab4_button42", "View Clustering")
           )
         )
       )        
