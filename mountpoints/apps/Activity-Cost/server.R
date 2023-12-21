79676d24134c2a6af5a5b15a296026352e459382
alexseymourdev <alexseymourdev@gmail.com>
2023-12-21 10:06:05 +0000

    rewrite global file and removing question mark character

--- mountpoints/apps/Activity-Cost/server.R
+++ mountpoints/apps/Activity-Cost/server.R
@@ -20,39 +20,33 @@ library(NHSRplotthedots)
 library(runcharter)
 # Shiny server
 server <- function(input, output, session) {
-  
+
   ## practice/pcn tab
   observe({
     if(input$switchView == FALSE) { # GP
       # Adjust logic for GP
       # GP page
       source("gpserver.R", local = TRUE)
-      
-    } else if(input$switchView == TRUE) { # PCN
-      
+
+    } else if (input$switchView == TRUE) { # PCN
+
       source("pcnserver.R", local = TRUE)
       # Adjust logic for PCN
     }
   })
-  
+
   ## PBC tab
   observe({
     if(input$switchViewPBC == FALSE) { # GP
       # Adjust logic for GP
       # GP page
       source("pbcserver.R", local = TRUE)
-      
-    } else if(input$switchViewPBC == TRUE) { # PCN
-      
+    } else if (input$switchViewPBC == TRUE) { # PCN
       source("pbcpcnserver.R", local = TRUE)
       # Adjust logic for PCN
     }
   })
-  
   # PBC page
-  
   #source("pbcserver.R", local = TRUE)
-  
+
 }
-
-
